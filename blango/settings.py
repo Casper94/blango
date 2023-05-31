78e9244059b86042b21a8413ef03a9a08b5dccbd
Saveen Shrestha <saveens.mail@gmail.com>
2023-05-31 05:11:50 +0000

    Finish django registation

--- blango/settings.py
+++ blango/settings.py
@@ -159,6 +159,9 @@ class Dev(Configuration):
     # ] 
     INTERNAL_IPS = ["192.168.11.179"]
 
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    ACCOUNT_ACTIVATION_DAYS = 7
+
 class Prod(Dev):
     DEBUG = False
     SECRET_KEY = values.SecretValue()
