6ec693ac838dfee6c0b2159c212a1d2661027a6e
Saveen Shrestha <saveens.mail@gmail.com>
2023-07-01 12:12:44 +0000

    Finish Serializers

--- blango/settings.py
+++ blango/settings.py
@@ -55,7 +55,9 @@ class Dev(Configuration):
         "allauth", 
         "allauth.account", 
         "allauth.socialaccount", 
-        "allauth.socialaccount.providers.google"
+        "allauth.socialaccount.providers.google", 
+
+        "rest_framework",
 
     ]
 
